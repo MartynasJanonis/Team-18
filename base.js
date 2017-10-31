21647816f90c200a9684ef32a2a3579b746611d1
Rod Matveev <rodka99@gmail.com>
2017-10-31 01:07:07 +0000

    turn off the lights

--- base.js
+++ base.js
@@ -2,14 +2,26 @@ $(document).ready(function() {
   var landingHeight = 0;
 
   landingHeight = $('#landing-height').height();
-  console.log("set current height on load = " + landingHeight);
+  historyHeight = $('#history').height();
+  console.log("landingHeight = " + landingHeight);
+  console.log("history height = " + historyHeight);
 
   $(window).scroll(function () {
-    if ($(this).scrollTop() > (landingHeight-300)) {
-      $('#section-text').text("History");
-    }
     if ($(this).scrollTop() < (landingHeight-300)) {
       $('#section-text').text("");
+      $('#section-text').css('color', 'white');
+      $('#cover').css('background-color', 'transparent');
+      $('.grid').css('border-left-color', '#CFD5DB');
+    }else if($(this).scrollTop() > (landingHeight-300) && $(this).scrollTop() < (landingHeight+historyHeight-300)) {
+      $('#section-text').text("History");
+      $('#section-text').css('color', 'white');
+      $('#cover').css('background-color', 'transparent');
+      $('.grid').css('border-left-color', '#CFD5DB');
+    }else if($(this).scrollTop() > (landingHeight+historyHeight-300)) {
+      $('#section-text').text("Research");
+      $('#section-text').css('color', 'black');
+      $('#cover').css('background-color', 'rgba(0,0,0,0.90)');
+      $('.grid').css('border-left-color', '#A9A9A9');
     }
   });
 });
