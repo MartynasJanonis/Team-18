294e08e6652e1eb3fabf6bcbe1f8c720edbbf48a
Rod Matveev <rodka99@gmail.com>
2017-10-31 11:07:01 +0000

    why is there a d in fridge but no d in refrigerator

--- base.js
+++ base.js
@@ -1,27 +1,35 @@
 $(document).ready(function() {
+  var addConstant = 0;
   var landingHeight = 0;
-
-  landingHeight = $('#landing-height').height();
-  historyHeight = $('#history').height();
+  var historyHeight = 0;
+  landingHeight = $('#landing-height').outerHeight();
+  historyHeight = $('#history').outerHeight();
   console.log("landingHeight = " + landingHeight);
   console.log("history height = " + historyHeight);
 
   $(window).scroll(function () {
-    if ($(this).scrollTop() < (landingHeight-300)) {
-      $('#section-text').text("");
-      $('#section-text').css('color', 'white');
+    console.log("scroll pos = " + $(this).scrollTop());
+    if ($(this).scrollTop() < (landingHeight+addConstant)) {
+      $('#section-text').text("History");
+      $('#section-text').css('color', 'transparent');
       $('#cover').css('background-color', 'transparent');
       $('.grid').css('border-left-color', '#CFD5DB');
-    }else if($(this).scrollTop() > (landingHeight-300) && $(this).scrollTop() < (landingHeight+historyHeight-300)) {
+      $('.logo').attr("src","/imgs/logo2.png");
+      $('.nav-button').css('color', 'black');
+    }else if($(this).scrollTop() > (landingHeight+addConstant) && $(this).scrollTop() < (landingHeight+historyHeight+addConstant)) {
       $('#section-text').text("History");
       $('#section-text').css('color', 'white');
       $('#cover').css('background-color', 'transparent');
       $('.grid').css('border-left-color', '#CFD5DB');
-    }else if($(this).scrollTop() > (landingHeight+historyHeight-300)) {
+      $('.logo').attr("src","/imgs/logo2.png");
+      $('.nav-button').css('color', 'black');
+    }else if($(this).scrollTop() > (landingHeight+historyHeight+addConstant)) {
       $('#section-text').text("Research");
       $('#section-text').css('color', 'black');
-      $('#cover').css('background-color', 'rgba(0,0,0,0.90)');
+      $('#cover').css('background-color', 'rgba(0,0,0,0.85)');
       $('.grid').css('border-left-color', '#A9A9A9');
+      $('.logo').attr("src","/imgs/logo2dark.png");
+      $('.nav-button').css('color', 'white');
     }
   });
 });
